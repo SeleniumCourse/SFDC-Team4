4a87176a3b9deaee84d3313a702e7f6adf140712
andreacastro <andrea.castro@jalasoft.com>
2015-06-29 13:38:33 -0400

    Create products

---
+++ src/main/java/com/selenium/course/pages/NewProductForm.java
@@ -0,0 +1,100 @@
+package com.selenium.course.pages;
+
+import com.selenium.course.framework.DriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+import static com.selenium.course.common.CommonUIMethods.check;
+import static com.selenium.course.common.CommonUIMethods.uncheck;
+
+/**
+ * Created by Andrea Castro on 6/12/2015.
+ */
+public class NewProductForm {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @FindBy(xpath = "//h1[contains(.,'Product Edit:')]")
+    @CacheLookup
+    WebElement ProductTitle;
+
+
+    @FindBy(id = "Name")
+    @CacheLookup
+    WebElement productNAmeField;
+
+    @FindBy(id = "ProductCode")
+    @CacheLookup
+    WebElement productCodeField;
+
+    @FindBy(xpath = "//textarea[@id='Description']")
+    @CacheLookup
+    WebElement productDescriptionField;
+
+    @FindBy(name = "save")
+    @CacheLookup
+    WebElement saveBtn;
+
+
+
+
+    public NewProductForm() {
+        this.driver = driver;
+        wait = DriverManager.getInstance().getWait();
+        driver = DriverManager.getInstance().getDriver();
+        PageFactory.initElements(driver, this);
+        try {
+            wait.withTimeout(3, TimeUnit.SECONDS).until(
+                    ExpectedConditions.visibilityOf(ProductTitle));
+        } catch (WebDriverException e) {
+            throw new WebDriverException(e);
+        } finally {
+            wait.withTimeout(15, TimeUnit.SECONDS);
+        }
+    }
+
+    public NewProductForm setNewProductName(String productName) {
+        productNAmeField.clear();
+        productNAmeField.sendKeys(productName);
+        return this;
+    }
+
+    public NewProductForm setProductDescriptionField(String productDescription) {
+        productDescriptionField.clear();
+        productDescriptionField.sendKeys(productDescription);
+        return this;
+    }
+
+    public NewProductForm setProductCodeField(String productCode) {
+        productCodeField.clear();
+        productCodeField.sendKeys(productCode);
+        return this;
+    }
+
+    public NewProductForm setStatus(boolean status) {
+        if (status) {
+            check(By.id("IsActive"));
+        } else {
+            uncheck(By.id("IsActive"));
+        }
+        return this;
+    }
+
+    public ProductDetail clickSave() {
+        saveBtn.click();
+        return new ProductDetail();
+    }
+
+
+}
+
