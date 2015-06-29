6487c9f7435cc024b97aede23e808bf3cab5b74a
andreacastro <andrea.castro@jalasoft.com>
2015-06-29 13:38:33 -0400

    Create products

---
+++ src/main/java/com/selenium/course/pages/ProductEditPage.java
@@ -0,0 +1,42 @@
+package com.selenium.course.pages;
+
+        import com.selenium.course.framework.DriverManager;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebDriverException;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.support.CacheLookup;
+        import org.openqa.selenium.support.FindBy;
+        import org.openqa.selenium.support.PageFactory;
+        import org.openqa.selenium.support.ui.ExpectedConditions;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+
+        import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Andrea Castro on 6/12/2015.
+ */
+public class ProductEditPage extends NewProductForm {
+
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(xpath = "//h1[contains(.,'Product Edit:')]")
+    @CacheLookup
+    WebElement EditProductTitle;
+
+
+    public ProductEditPage() {
+        this.driver = driver;
+        wait = DriverManager.getInstance().getWait();
+        driver = DriverManager.getInstance().getDriver();
+        PageFactory.initElements(driver, this);
+        wait.withTimeout(3, TimeUnit.SECONDS).until(
+                ExpectedConditions.visibilityOf(EditProductTitle));
+        wait.withTimeout(15, TimeUnit.SECONDS);
+    }
+
+
+}
+
+
---
+++ src/test/java/com/selenium/course/testng/EditProduct.java
@@ -0,0 +1,71 @@
+package com.selenium.course.testng;
+
+import com.selenium.course.pages.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.junit.Assert.assertTrue;
+/**
+ * Created by Andrea Castro on 6/29/2015.
+ */
+public class EditProduct {
+
+    /**
+     * Created by Andrea Castro on 6/12/2015.
+     */
+
+    private LoginPage loginPage;
+    private MainApp mainApp;
+    private PageMenuBar pageMenuBar;
+    private ProductsPage productsPage;
+    private NewProductForm newProductForm;
+    private ProductDetail productDetail;
+    private ProductEditPage productEditPage;
+    String productName,productDescription,productCode;
+
+    @BeforeClass
+    public void setUp() {
+
+        productName = "Product1Test";
+        productDescription = "This product is a test to create new products";
+        productCode = "P2015";
+
+        loginPage = new LoginPage();
+        mainApp = loginPage.loginAsPrimaryUser();
+        pageMenuBar = mainApp.goToPageMenuBar();
+        productsPage = pageMenuBar.clickProducts();
+        newProductForm = productsPage.clickNew();
+        productDetail = newProductForm.setNewProductName(productName).
+                setProductDescriptionField(productDescription).setProductCodeField(productCode).setStatus(true).clickSave();
+    }
+
+    @Test
+    public void testUntitled() {
+
+        String productName= "Newproductname";
+        String productDescription = "new description this product is a test to create new products";
+        String productCode= "NewP20151";
+
+
+        productEditPage = productDetail.clickEdit();
+        productDetail = productEditPage.setNewProductName(productName).setProductDescriptionField(productDescription)
+                .setProductCodeField(productCode).setStatus(false).clickSave();
+
+
+        assertTrue(productDetail.verifyName(productName));
+        assertTrue(productDetail.verifyDescription(productDescription));
+
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+
+        productDetail.clickDelete();
+        pageMenuBar = mainApp.goToPageMenuBar();
+
+    }
+}
+
+
+
